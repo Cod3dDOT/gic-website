a1d6543ec7daff45895956c2afd460c37bea74f3
Cod3d <vsevolodkutsyn@gmail.com>
2022-08-06 12:09:26 +0300

    Allow vercel deployments only on main branch

---
+++ .ignore-build-step.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [["$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  # Proceed with the build
+    echo "Proceeding with build on main branch"
+    exit 1;
+
+else
+    # Don't build
+    echo "Not building on $VERCEL_GIT_COMMIT_REF branch"
+    exit 0;
+fi
