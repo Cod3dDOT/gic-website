499d581f34a5756852daec68bd9534c5b79e4ecd
Cod3d <vsevolodkutsyn@gmail.com>
2022-08-06 12:36:50 +0300

    Fixed .ignore-build-step.sh

--- .ignore-build-step.sh
+++ .ignore-build-step.sh
@@ -2,13 +2,12 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [["$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
-  # Proceed with the build
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
+    # Proceed with the build
     echo "Proceeding with build on main branch"
-    exit 1;
-
+    exit 1
 else
     # Don't build
     echo "Not building on $VERCEL_GIT_COMMIT_REF branch"
-    exit 0;
+    exit 0
 fi
