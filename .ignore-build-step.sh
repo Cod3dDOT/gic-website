ec47fa8a9d9733cf77e119f45aa2421fac2e1004
Cod3d <vsevolodkutsyn@gmail.com>
2022-08-08 22:07:35 +0300

    Updated .ignore-build-step.sh

--- .ignore-build-step.sh
+++ .ignore-build-step.sh
@@ -3,6 +3,15 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
 if [[ "$VERCEL_GIT_COMMIT_REF" == "main" ]]; then
+
+    git diff HEAD^ HEAD --name-only '!*.md' '!.husky' '!.vscode'
+
+    if [[ $(git diff HEAD^ HEAD --name-only '!*.md' '!.husky' '!.vscode') != "" ]]; then
+        exit 1
+    else
+        :
+    fi
+
     # Proceed with the build
     echo "Proceeding with build on main branch"
     exit 1
